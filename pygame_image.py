c766dcf010ab53502c34fd089f36f7e87e94c886
c0b220385b <c0b220385b@edu.teu.ac.jp>
2023-06-13 16:49:23 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -14,11 +14,12 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [1600-tmr, 0])
         screen.blit(bird3_imgs[tmr%2], [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 
