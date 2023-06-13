94de05ba62788d1facf340c7c5e737de9f92a0cd
c0b220385b <c0b220385b@edu.teu.ac.jp>
2023-06-13 16:40:02 +0900

    練習2

--- pygame_image.py
+++ pygame_image.py
@@ -9,16 +9,11 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     bird3_img = pg.image.load("ex01/fig/3.png")
     bird3_img = pg.transform.flip(bird3_img, True, False)
-    bird3_ang10_img = pg.transform.rotate(bird3_img, 10)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [0, 0])
-        if tmr % 2 == 0:
-            screen.blit(bird3_img, [300, 200])
-        else:
-            screen.blit(bird3_ang10_img, [300, 200])
+            screen.blit(bg_img, [0, 0])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
